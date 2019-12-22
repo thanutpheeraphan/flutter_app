421ecf8bd148d45734e48ec2e2966067346968d4
thanut <thanutza_dmex@gmail.com>
2019-12-22 23:32:38 +0700

    New Commit

--- flutter_app2/README.md
+++ flutter_app2/README.md
@@ -1,4 +1,4 @@
-# flutter_app
+# flutter_app2
 
 A new Flutter application.
 
--- flutter_app2/android/app/src/main/kotlin/com/example/flutter_app2/MainActivity.kt
+++ flutter_app2/android/app/src/main/kotlin/com/example/flutter_app2/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_app
+package com.example.flutter_app2
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
