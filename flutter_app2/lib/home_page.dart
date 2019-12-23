b93b838013ce5dbc143f6f0a041efbe92281efef
thanut <thanutza_dmex@gmail.com>
2019-12-24 05:06:10 +0700

    Changed up GridView
    continue tmr

--- flutter_app2/lib/home_page.dart
+++ flutter_app2/lib/home_page.dart
@@ -4,30 +4,45 @@ import 'package:flutter_app2/secondroute.dart';
 
 class home_page extends StatefulWidget {
   @override
-  _home_pageState createState() => _home_pageState();
+  MyHomePage createState() => MyHomePage();
 }
 
 class _home_pageState extends State<home_page> {
   @override
   Widget build(BuildContext context) {
-    List<String> widgetList = ['A', 'B', 'C'];
-    var size = MediaQuery.of(context).size;
     var spacecrafts = ["Scientific Programme","Personal Programme","Programme Overview","Kepler","Juno","Casini","Columbia","Challenger","Huygens"];
+    var title_list = ["Scientific Programme","Personal Programme","Programme Overview","Authors","Posters","Venues & Plans","Exhibition","General Information","Programme of Events","Partners","Networking","News"];
+    var icon_lists = [Icons.ac_unit,Icons.supervised_user_circle];
+    var new_cards = new Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const ListTile(
+            leading: Icon(Icons.ac_unit,size:20),
+            title:Text("Snow Flake"),
+            subtitle: Text("Beyboy"),
+          )
+        ],
+      ),
+    );
+
+
+
+
     var myGridView = new GridView.builder(
       shrinkWrap: true,
-
       itemCount: spacecrafts.length,
       gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
       itemBuilder: (BuildContext context, int index) {
         return new SingleChildScrollView(
            child: GestureDetector(
-          child: new Card(
-          elevation: 3.0,
-          child: new Container(
-            alignment: Alignment.centerLeft,
-            margin: new EdgeInsets.only(top: 8.0, bottom: 8.0,left: 8.0),
-            //childAspectRatio: (itemWidth / itemHeight),
-            child: new Text(spacecrafts[index]),
+             child: new Card(
+               elevation: 3.0,
+               child: new Container(
+                 alignment: Alignment.centerLeft,
+                 margin: new EdgeInsets.only(top: 8.0, bottom: 8.0,left: 8.0),
+                 child: new Text(spacecrafts[index]),
+
           ),
         ),
         onTap: () {
@@ -42,60 +57,13 @@ class _home_pageState extends State<home_page> {
         );
       },
     );
-    var newGrid = new Scaffold(
-
-        body: new Container(
-          child: new GridView.count(
-            crossAxisCount: 3,
-            controller: new ScrollController(keepScrollOffset: false),
-            shrinkWrap: true,
-            scrollDirection: Axis.vertical,
-            children: widgetList.map((String value) {
-              return new Container(
-
-                color: Colors.green,
-                margin: new EdgeInsets.all(1.0),
-                child: new Center(
-                  child: new Text(
-                    value,
-                    style: new TextStyle(fontSize: 50.0,color: Colors.white),
-                  ),
-                ),
-              );
-            }).toList(),
-          ),
-        )
-    );
-    Widget temp() => new Scaffold(
-        body: new Container(
-          child: new GridView.count(
-            crossAxisCount: 3,
-            controller: new ScrollController(keepScrollOffset: false),
-            shrinkWrap: true,
-            scrollDirection: Axis.vertical,
-            children: widgetList.map((String value) {
-              return new Container(
-
-                color: Colors.green,
-                margin: new EdgeInsets.all(1.0),
-                child: new Center(
-                  child: new Text(
-                    value,
-                    style: new TextStyle(fontSize: 50.0,color: Colors.white),
-                  ),
-                ),
-              );
-            }).toList(),
-          ),
-        )
-    );
     return Scaffold(
       body: Center(
 
           child: ColumnSuper(
             innerDistance: 4.0,
             outerDistance: 4.0,
-            children: [Image.asset('assets/images/demo-2.jpg'),myGridView
+            children: [Image.asset('assets/images/demo-2.jpg'),
 
             ],
           ),
@@ -109,5 +77,26 @@ class _home_pageState extends State<home_page> {
 }
 
 
-
+class MyHomePage extends State<home_page>{
+  @override
+  Widget build(BuildContext context){
+    var title_list = ["Scientific Programme","Personal Programme","Programme Overview","Authors","Posters","Venues & Plans","Exhibition","General Information","Programme of Events","Partners","Networking","News"];
+    var icon_lists = [Icons.ac_unit,Icons.supervised_user_circle];
+    return new Scaffold(
+      body: new GridView.count(
+        crossAxisCount: 4,
+        children: new List<Widget>.generate(12, (index) {
+          return new GridTile(
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/images/demo-2.jpg'),
+                new Text(title_list[index],
+              )],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
 
