c755a4ca8ff1e7558e64a94de0d9c421d7937ba8
thanut <thanutza_dmex@gmail.com>
2020-01-06 04:04:18 +0700

    Put in columnsuper
    added super in column

--- flutter_app2/lib/main2.dart
+++ flutter_app2/lib/main2.dart
@@ -148,7 +148,7 @@ class _HomeState extends State<Home> {
     var store_grid = new Container(
       child: myGridView,
     );
-    return new Column(
+    return new ColumnSuper(
       //separatorOnTop: true,
 
 
