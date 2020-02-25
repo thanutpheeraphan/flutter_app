1b6e83587b601ef2c3c0e0aec7860120f39e5fc8
thanut <thanutza_dmex@gmail.com>
2020-02-26 00:05:24 +0700

    Put in tabs for 3 card layout

--- flutter_app2/lib/ProgramOverview3/third_route.dart
+++ flutter_app2/lib/ProgramOverview3/third_route.dart
@@ -4,20 +4,64 @@ import 'package:flutter/cupertino.dart';
 class third_route extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Programme Overview"),
+    List<Widget> containers = [
+      Container(
+        color: Colors.amberAccent,
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-            Navigator.pop(context);
+      Container(
+        color: Colors.cyanAccent,
+      ),
+      Container(
+        color: Colors.pinkAccent,
+      ),
 
-          },
-          child: Text('Go back!'),
+    ];
+
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: <Widget>[ Tab(
+                child: Container(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text("25/03/20"),
+                  ),
+                ),
+              ),
+                Tab(
+                  child: Container(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("26/03/20"),
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("27/03/20"),
+                    ),
+                  ),
+                ),
+                ],
+            ),
+            title: Text("Tabs Demo"),
+          ),
+          body: TabBarView(
+            children: containers
+          ),
         ),
-      ),
     );
   }
 }
+
+//
+//<Widget>[
+//
+//Icon(Icons.directions_car),
+//Icon(Icons.directions_transit),
+//Icon(Icons.directions_bike),
+//],
